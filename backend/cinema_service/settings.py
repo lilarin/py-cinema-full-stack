62761aa42a91634c0367d9447a7e3c0c6edc2c2d
lilarin <80650911+lilarin@users.noreply.github.com>
2024-08-16 09:40:27 +0300

    Another one minor refinement of settings.py

--- backend/cinema_service/settings.py
+++ backend/cinema_service/settings.py
@@ -182,7 +182,7 @@ SPECTACULAR_SETTINGS = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60 * 60),
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=1),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": False,
 }
